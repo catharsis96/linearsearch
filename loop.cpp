92b840a1198de2db452ea26b8ca7be9e50a28598
catharsis96 <catharsis96@yandex.ru>
2015-03-04 15:57:29 +0300

    Added loop

---
+++ loop.cpp
@@ -0,0 +1,17 @@
+// сумма чисел от 1 до 1000.
+#include <iostream>
+#include <clocale>
+using namespace std;
+int main()
+{
+	int i; //счетчик цикла
+	int sum = 0; //обнуляем переменную
+	//задаем начальное значение 1, конечное 1000 и задаем шаг цикла 1
+	for(i = 1; i <= 1000; i++)
+	{
+		sum = sum + i;
+	}
+	setlocale(LC_ALL, "russian");
+	cout << "Сумма чисел от 1 до 1000: " << sum;
+	return 0;
+}
