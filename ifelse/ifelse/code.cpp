2cad95f4f13187b9e3b80d5d31f51ec6957ddecd
catharsis96 <catharsis96@yandex.ru>
2015-03-05 18:43:17 +0300

    business logic

---
+++ ifelse/ifelse/code.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	int a,b,res;
+	char op;
+	cout << "type a->" << endl;
+	cin >> a;
+	cout << "type b->" << endl;
+	cin >> b;
+	cout << "type op->" << endl;
+	//op - операция (+,-,/,*)
+	cin >> op;
+	switch(op)
+	{
+	case '+': res = a+b; cout << "res = " << res << endl; break;
+	case '-': res = a-b; cout << "res = " << res << endl; break;
+	case '*': res = a*b; cout << "res = " << res << endl; break;
+	default : cout << "Unknown oparator" << endl;
+	}
+}
